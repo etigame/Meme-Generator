0aeb9641a528c22aa0f6041a33c8787cef30ef9a
Eti Gal-Or <etim139@gmail.com>
2022-10-26 16:29:00 +0300

    connect to storage

--- js/meme-service.js
+++ js/meme-service.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const GMEME_STORAGE_KEY = 'gMeme_db'
+
 const gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
@@ -22,18 +24,22 @@ function getMeme() {
 
 function setLineTxt(txt) {
     gMeme.lines[gMeme.selectedLineIdx].txt = txt
+    saveMemeToStorage()
 }
 
 function setImg(imgId) {
     gMeme.selectedImgId = imgId
+    saveMemeToStorage()
 }
 
 function setColor(color) {
     gMeme.lines[gMeme.selectedLineIdx].color = color
+    saveMemeToStorage()
 }
 
 function changeFontSize(delta) {
     gMeme.lines[gMeme.selectedLineIdx].size += delta
+    saveMemeToStorage()
 }
 
 function addLine() {
@@ -46,8 +52,14 @@ function addLine() {
         pos: {offsetX: 100, offsetY: 400} // offsetY should be not specific
     }
     gMeme.lines.push(newLine)
+    saveMemeToStorage()
 }
 
 function updateSelectedLine() {
     gMeme.selectedLineIdx = 1 // should be base on position x and y of the line
+    saveMemeToStorage()
+}
+
+function saveMemeToStorage() {
+    saveToStorage(GMEME_STORAGE_KEY, gMeme)
 }
