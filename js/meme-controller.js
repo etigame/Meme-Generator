47bf28eebc1ed212071fdf501e1e878ea6fb6c2e
Eti Gal-Or <etim139@gmail.com>
2022-10-26 11:39:00 +0300

    first meme

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,3 +1,29 @@
 'use strict'
 
-console.log('hello');
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.getElementById('my-canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+}
+
+
+function renderMeme() {
+    const img = new Image() 
+    img.src = 'img/2.jpg' 
+    img.onload = () => {
+      gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+      
+      gCtx.lineWidth = 2
+      gCtx.strokeStyle = 'black'
+      gCtx.fillStyle = 'white'
+  
+      gCtx.font = '40px Arial'
+      gCtx.fillText('hello world', 100, 100) 
+      gCtx.strokeText('hello world', 100, 100) 
+    }
+
+}
+
