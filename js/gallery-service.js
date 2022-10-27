d2db46ce4ccfe116811363949d568977c645d654
Eti Gal-Or <etim139@gmail.com>
2022-10-27 12:42:39 +0300

    main layout to gallery page

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -3,7 +3,29 @@
 const gImgs = [
     {id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']},
     {id: 2, url: 'img/2.jpg', keywords: ['funny', 'cat']},
-    {id: 3, url: 'img/3.jpg', keywords: ['funny', 'cat']}
+    {id: 3, url: 'img/3.jpg', keywords: ['funny', 'cat']},
+    {id: 4, url: 'img/4.jpg', keywords: ['funny', 'cat']},
+    {id: 5, url: 'img/5.jpg', keywords: ['funny', 'cat']},
+    {id: 6, url: 'img/6.jpg', keywords: ['funny', 'cat']},
+    {id: 7, url: 'img/7.jpg', keywords: ['funny', 'cat']},
+    {id: 8, url: 'img/8.jpg', keywords: ['funny', 'cat']},
+    {id: 9, url: 'img/9.jpg', keywords: ['funny', 'cat']},
+    {id: 10, url: 'img/10.jpg', keywords: ['funny', 'cat']},
+    {id: 11, url: 'img/11.jpg', keywords: ['funny', 'cat']},
+    {id: 12, url: 'img/12.jpg', keywords: ['funny', 'cat']},
+    {id: 13, url: 'img/13.jpg', keywords: ['funny', 'cat']},
+    {id: 14, url: 'img/14.jpg', keywords: ['funny', 'cat']},
+    {id: 15, url: 'img/15.jpg', keywords: ['funny', 'cat']},
+    {id: 16, url: 'img/16.jpg', keywords: ['funny', 'cat']},
+    {id: 17, url: 'img/17.jpg', keywords: ['funny', 'cat']},
+    {id: 18, url: 'img/18.jpg', keywords: ['funny', 'cat']},
+    {id: 19, url: 'img/19.jpg', keywords: ['funny', 'cat']},
+    {id: 20, url: 'img/20.jpg', keywords: ['funny', 'cat']},
+    {id: 21, url: 'img/21.jpg', keywords: ['funny', 'cat']},
+    {id: 22, url: 'img/22.jpg', keywords: ['funny', 'cat']},
+    {id: 23, url: 'img/23.jpg', keywords: ['funny', 'cat']},
+    {id: 24, url: 'img/24.jpg', keywords: ['funny', 'cat']},
+    {id: 25, url: 'img/25.jpg', keywords: ['funny', 'cat']}
 ];
 
 function getImages() { 
