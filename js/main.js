b806a8e9e5b98ec7bd91cbd2a4ebd07ec5ea385c
Eti Gal-Or <etim139@gmail.com>
2022-10-29 15:02:51 +0200

    add responsive for gallery page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict'
+
+function onToggleMenu() {
+    const elNav = document.querySelector('.main-nav')
+    const elBody = document.querySelector('body')
+    const elBtn = document.querySelector('.open-menu-btn')
+  
+    const isOpen = elNav.classList.contains('display-menu')
+  
+    if (!isOpen) {
+      elBody.classList.add('menu-open')
+      elNav.classList.add('display-menu')
+      elBtn.innerText = 'X'
+    } else {
+      elBody.classList.remove('menu-open')
+      elNav.classList.remove('display-menu')
+      elBtn.innerText = '☰'
+    }
+  }
