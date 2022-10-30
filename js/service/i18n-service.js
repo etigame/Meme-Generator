a93e23616edc319a989e7fddf0576841036dde6a
Eti Gal-Or <etim139@gmail.com>
2022-10-30 20:33:43 +0200

    fix footer and more little changes

---
+++ js/service/i18n-service.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const gTrans = {
+    'title': {
+        en: 'Todo',
+        he: 'משימות'
+    },
+    'subtitle': {
+        en: 'MVC - Model-View-Controller',
+        he: 'מודל - ויו - קונטרולר',
+    },
+    'filter-all': {
+        en: 'All',
+        he: 'הכל',
+    },
+    'filter-active': {
+        en: 'Active',
+        he: 'פעיל'
+    },
+    'filter-done': {
+        en: 'Done',
+        he: 'הושלם',
+    },
+    'stat-todo-label': {
+        en: 'Todo',
+        he: 'לעשות',
+    },
+    'stat-active-label': {
+        en: 'Active',
+        he: 'פעיל',
+    },
+    'add': {
+        en: 'Add',
+        he: 'הוסף',
+    },
+    'sure': {
+        en: 'Are you sure?',
+        es: 'Estas Seguru?',
+        he: 'בטוח נשמה?',
+    },
+    'add-todo-placeholder': {
+        en: 'What needs to be done?',
+        es: 'Que te tienes que hacer?',
+        he: 'מה יש לעשות?'
+    }
+}
